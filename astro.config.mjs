fee8f8198498557754448e2bcd16d28b99d422dc
Pranjal Rane <rane.pr@northeastern.edu>
2024-05-08 17:32:45 -0400

    deployment token update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pranjal0207.github.io/',
-  base: '',
+  site: 'https://pranjal0207.github.io',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
