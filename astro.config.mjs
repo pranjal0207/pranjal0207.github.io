2ffcea468169790fdb999207a9c9f1ad8a0db465
Pranjal Rane <rane.pr@northeastern.edu>
2024-05-08 16:54:29 -0400

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: '`https://pranjal0207.github.io/',
+  site: 'https://pranjal0207.github.io/',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
